1e3b12a796504c29011a69c83c4fb9ece26de7d3
RITADEDOLAPO <ritaadedolapo2@gmail.com>
2022-10-24 19:26:21 -0700

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,13 +8,13 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
+	int t;
 
 	for (t = 0; t < n; t++)
 	{
 		printf("%d", a[t]);
 		if (t != n - 1)
-			printf(",");
+			printf(", ");
 	}
 
 	printf("\n");
